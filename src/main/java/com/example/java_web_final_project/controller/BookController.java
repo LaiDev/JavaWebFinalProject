7733fc8a263cc71023f781637e75503857fea257
Aalaizha B <l.bnjamin0309@gmail.com>
2023-06-06 18:42:17 -0400

    ADD: Enable CORS

--- src/main/java/com/example/java_web_final_project/controller/BookController.java
+++ src/main/java/com/example/java_web_final_project/controller/BookController.java
@@ -4,12 +4,14 @@ import com.example.java_web_final_project.model.Book;
 import com.example.java_web_final_project.service.BookService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
 @Controller
 @ResponseBody
 @RequestMapping("/books")
+@CrossOrigin(origins = "http://localhost:63342")
 public class BookController {
 
     private final BookService bookService;
